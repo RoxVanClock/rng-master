49cb3bc6980a3c38de40882e2a09af418de43aa2
RoxVanClock <keyroxas34@gmail.com>
2026-03-13 12:13:46 +0100

    Create data-locations.js

---
+++ pages/data-locations.js
@@ -0,0 +1,13 @@
+const DATA_LOCATIONS = {
+    "em_route101": {
+        name: "Route 101 (Emeraude)",
+        game: "emerald",
+        type: "wild",
+        // Les 12 slots officiels de PokéFinder pour cette route
+        slots: [
+            "Chenipotte", "Zigzaton", "Chenipotte", "Zigzaton", "Chenipotte", 
+            "Chenipotte", "Zigzaton", "Zigzaton", "Zigzaton", "Zigzaton", 
+            "Zigzaton", "Zigzaton"
+        ]
+    }
+};
